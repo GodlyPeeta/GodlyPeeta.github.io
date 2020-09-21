bb08245e5e81a96e982f999150fe478729b0bc02
GodlyPeeta <peterzhou561@gmail.com>
2020-09-21 11:33:21 -0400

    sell books added

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
-var clicks = 0;
+var money = 0;
+
+var materialNames = ['Paper'];
+var materialValues = [5000]
+
 function sellBook() {
-    clicks += 1;
-    document.getElementsByClassName("count")[0].innerHTML = "Money: $" + (Math.round(clicks * 100) / 100).toFixed(2);
-    ;
+    paper = Math.round(Math.random()*200)+50
+    materialValues[0]-=paper
+    cost = Math.round(Math.random()*300)/100+9
+    money += cost;
 };
+
+function refreshMaterials() {
+    var names =""
+    var values =""
+    for(i=0; i<materialNames.length; i++){
+        names += materialNames[i] + "<br>";
+        values += materialValues[i] + "<br>";
+        document.getElementsByClassName("materialNames")[0].innerHTML = names;
+        document.getElementsByClassName("materialValues")[0].innerHTML = values;
+    }
+}
+
+function refreshMoney() {
+    document.getElementsByClassName("count")[0].innerHTML = "Money: $" + (Math.round(money * 100) / 100).toFixed(2);
+}
+
+function tick() {
+    refreshMoney()
+    refreshMaterials()
+    console.log("tick")
+}
+
+function runTicks() {
+    setInterval(tick, 50);
+}
